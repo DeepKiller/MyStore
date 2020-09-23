04bd91fc5845247a282a37cf30a0d2cbefc8ff37
DeepKiller <deep.killer@yandex.by>
2020-09-23 20:01:03 +0300

    Base functions added

--- PYTHON/PyGame/Snake/Game.py
+++ PYTHON/PyGame/Snake/Game.py
@@ -1,17 +1,55 @@
 import pygame
 import random
-WIDTH = 1000
-HEIGHT = 1000
-FPS = 60
+import Tile
+
+WIDTH = 500
+HEIGHT = 500
+FPS = 5
+
+pygame.init()
 pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
 clock = pygame.time.Clock()
 running = True
+screen.fill((255,255,255))
+AllSprite = pygame.sprite.Group()
+Player = Tile.Tile(10,10,WIDTH/2,HEIGHT/2,(0,255,0))
+Borders = list()
+#Borders.append(Player)
+i=0
+while i <= HEIGHT:
+    Borders.append(Tile.Tile(10,10,i,0,(0,0,0)))
+    Borders.append(Tile.Tile(10,10,i,WIDTH,(0,0,0)))
+    i+=10
+i=0
+while i<= WIDTH:
+    Borders.append(Tile.Tile(10,10,0,i,(0,0,0)))
+    Borders.append(Tile.Tile(10,10,WIDTH,i,(0,0,0)))
+    i+=10
+for i in Borders:
+    AllSprite.add(i)
+    i.Direction=""
+Food = Tile.Food(10,10,WIDTH,HEIGHT)
+AllSprite.add(Food)
+AllSprite.add(Player)
 while running:
-    screen.fill((255,0,0))
-    pygame.display.flip()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                Player.ChangeDirection("L")
+            if event.key == pygame.K_RIGHT:
+                Player.ChangeDirection("R")
+            if event.key == pygame.K_UP:
+                Player.ChangeDirection("U")
+            if event.key == pygame.K_DOWN:
+                Player.ChangeDirection("D")
+    if Player.CheckLose(Borders):
+        Player.rect.center=(250,250)
+    pygame.display.flip()
+    AllSprite.update()
+    screen.fill((255,255,255))
+    AllSprite.draw(screen)
     clock.tick(FPS)
 pygame.quit()
---
+++ PYTHON/PyGame/Snake/Tile.py
@@ -0,0 +1,44 @@
+import pygame
+import random
+
+class Tile(pygame.sprite.Sprite):
+    Size = (0,0)
+    Position = (0,0)
+    Color = (0,0,0)
+    Direction = "U" # U = UP D = Down L = Left R = Right
+    def __init__(self,SizeV,SizeH,PosY,PosX,Col):
+        self.Size = (SizeV,SizeH)
+        self.Position = (PosX,PosY)
+        self.Color = Col
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface(self.Size)
+        self.image.fill(self.Color)
+        self.rect = self.image.get_rect()
+        self.rect.center = (self.Position)
+    
+    def ChangeDirection(self,NewDir):
+        self.Direction = NewDir
+
+    def CheckLose(self,Tiles):
+        return pygame.sprite.spritecollide(self, Tiles, False)          
+
+
+    def update(self):
+        if self.Direction == "U":
+            self.rect.y-=self.Size[0]
+        if self.Direction == "D":
+            self.rect.y+=self.Size[0]
+        if self.Direction == "R":
+            self.rect.x+=self.Size[1]
+        if self.Direction == "L":
+            self.rect.x-=self.Size[1]
+
+class Food(pygame.sprite.Sprite):
+    Size =(0,0)
+    def __init__(self,SizeV,SizeH,WIDTH,HEIGTH):
+        self.Size = (SizeV,SizeH)
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface(self.Size)
+        self.image.fill((255,127,127))
+        self.rect = self.image.get_rect()
+        self.rect.center = (random.randint(0+10,WIDTH-10),random.randint(0+10,HEIGTH-10))
