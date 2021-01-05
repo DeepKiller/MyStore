d6e6bf0f3246885618c66e02955190e142d1d1bf
DeepKiller <deep.killer@yandex.by>
2021-01-05 22:54:33 +0300

    C++ upload

--- C++/Project 1/main.cpp
+++ C++/Project 1/main.cpp
@@ -11,7 +11,11 @@ class Server{
         if(WSAStartup(MAKEWORD(2,2),&wData)==0){
             printf("WSA startup");
         }
+        sockaddr_in addr;
+        int addrl = sizeof(addr);
+        
     }
     private:
         WSADATA wData;
+        SOCKET Socket;
 };
